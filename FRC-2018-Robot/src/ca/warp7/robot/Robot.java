225b4be75d64b75ac1768f4016b3dc2c7bd94a1b
Brian Petkovsek <bpetkov@bell.net>
2018-01-10 16:35:35 -0500

    Code

---
+++ FRC-2018-Robot/src/ca/warp7/robot/Robot.java
@@ -0,0 +1,27 @@
+package ca.warp7.robot;
+
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class Robot extends SampleRobot {
+	
+	@Override
+	public void robotInit() {
+	
+	}
+
+	@Override
+	public void autonomous() {
+	
+	}
+	@Override
+	public void operatorControl() {
+	
+	}
+	@Override
+	public void test() {
+	
+	}
+}
