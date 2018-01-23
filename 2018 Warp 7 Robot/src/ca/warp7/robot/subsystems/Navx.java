b58ecbab6afc5218fbd45d27a439e05db0324310
Ryan Prairie <prairie421@gmail.com>
2018-01-23 16:16:35 -0500

    started distance calculation function

--- 2018 Warp 7 Robot/src/ca/warp7/robot/subsystems/Navx.java
+++ 2018 Warp 7 Robot/src/ca/warp7/robot/subsystems/Navx.java
@@ -2,10 +2,17 @@ package ca.warp7.robot.subsystems;
 
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
-
+import edu.wpi.first.wpilibj.Timer;
 
 public class Navx {
 	private AHRS ahrs;
+	
+	//variables to calculate distace and velocity
+	private double time = Timer.getMatchTime();
+	private double velX = 0.0;
+	private double velY = 0.0;
+	private double distX = 0.0;
+	private double distY = 0.0;
 	
 	public Navx(){
 		ahrs = new AHRS(SPI.Port.kMXP);
@@ -17,6 +24,7 @@ public class Navx {
 			System.out.println("Calibrating Navx");
 		}
 	}
+	
 	public double getAccelX() {
 		return ahrs.getRawAccelX();
 	}
@@ -25,4 +33,12 @@ public class Navx {
 		return ahrs.getRawAccelY();
 	}
 	
+	public double getDistX() {
+		double temp = Timer.getMatchTime() - time;
+		time = temp + time;
+		
+		
+		
+		return 1.0;
+	}
 }
