7fd127d04db67496a6b5b73e875d77985f06ddc9
Oasixer <kaelan.ms@gmail.com>
2018-04-07 12:43:44 -0400

    more trajectory auto testing/fixing

--- 2018 Warp 7 Robot/src/ca/warp7/robot/auto/TrajectoryAuto.java
+++ 2018 Warp 7 Robot/src/ca/warp7/robot/auto/TrajectoryAuto.java
@@ -10,7 +10,7 @@ import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.Waypoint;
 import jaci.pathfinder.modifiers.TankModifier;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class TrajectoryAuto {
 	private int ticks=0;
 	private File left,right;
@@ -34,7 +34,7 @@ public class TrajectoryAuto {
         };
 
         Trajectory trajectory = Pathfinder.generate(points, config);
-
+        Trajectory.Segment seg0 = trajectory.get(0);
         // Wheelbase Width = 0.5m
         TankModifier modifier = new TankModifier(trajectory).modify(0.5);
 		
@@ -42,29 +42,42 @@ public class TrajectoryAuto {
         rightTrajectory = modifier.getRightTrajectory();
 			
 			notifier = new Notifier(this::executeTrajectory);
-			notifier.startPeriodic(0.05);
+			notifier.startPeriodic(seg0.dt);
 			drive.resetDistance();
 			navx.resetAngle();
 	}
 	
 	private void executeTrajectory(){
 		if (ticks<leftTrajectory.length()-1) {
-		double driveVLeft=drive.leftVelocity()*-0.01;
-		double driveVRight=drive.leftVelocity()*-0.01;
+		double driveVLeft=drive.leftVelocity()*-100; //convert from backwards centimeters to forward meters
+		double driveVRight=drive.leftVelocity()*-100; //convert from backwards centimeters to forward meters
 		System.out.println(ticks);
 		Trajectory.Segment segLeft = leftTrajectory.get(ticks);
 		Trajectory.Segment segRight = rightTrajectory.get(ticks);
 		double errorVLeft=segLeft.velocity-driveVLeft;
 		double errorVRight=segRight.velocity-driveVRight;
+		double errorDLeft=segLeft.position-driveVLeft;
+		double errorDRight=segRight.velocity-driveVRight;
 		double ffLeftAcc=segLeft.acceleration/ka;
 		double ffRightAcc=segRight.acceleration/ka;
 		double ffLeftVel=segLeft.velocity/kv;
 		double ffRightVel=segRight.velocity/kv;
-		double leftNewPower=ffLeftVel+ffLeftAcc+errorVLeft*p;
-		double rightNewPower=ffRightVel+ffRightAcc+errorVRight*p;
-		System.out.println("leftP= "+leftNewPower+" rightP= "+ rightNewPower + "rightV= "+driveVRight + " rightVSP= "+segRight.velocity);
+		double leftNewPower=ffLeftVel+ffLeftAcc+errorDLeft*p;
+		double rightNewPower=ffRightVel+ffRightAcc+errorDRight*p;
 		drive.tankDrive(leftNewPower, rightNewPower);
-		
+		System.out.printf("%f,%f,%f,%f,%f,%f,%f,%f\n", 
+		        segLeft.dt, segLeft.x, segLeft.y, segLeft.position, segLeft.velocity, 
+		            segLeft.acceleration, segLeft.jerk, segLeft.heading);
+		//System.out.println("leftP= "+leftNewPower+" rightP= "+ rightNewPower + "rightV= "+driveVRight + " rightVSP= "+segRight.velocity);
+		SmartDashboard.putNumber("current leftV",driveVLeft);
+		SmartDashboard.putNumber("current rightV",driveVRight);
+		SmartDashboard.putNumber("leftV Setpoint",segLeft.velocity);
+		SmartDashboard.putNumber("rightV Setpoint",segRight.velocity);
+		//SmartDashboard.putNumber("current leftA",seg);
+		//SmartDashboard.putNumber("current rightA",);
+		SmartDashboard.putNumber("leftA Setpoint",segLeft.acceleration);
+		SmartDashboard.putNumber("rightA Setpoint",segRight.acceleration);
+		SmartDashboard.putNumber("current left distance",drive.getLeftDistance());
 		ticks++;
 		}
 	}
